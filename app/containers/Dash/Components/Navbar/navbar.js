88bf8a57e86521771e478438f06f3ffda5efa968
aashish <aashishshrestha12@gmail.com>
2019-05-03 13:07:45 +0545

    Dash updated

--- app/containers/Dash/Components/Navbar/navbar.js
+++ app/containers/Dash/Components/Navbar/navbar.js
@@ -1,14 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './navbar.css';
 
-class Navbar extends Component{
-    render(){
-        return(
-            <div>Navbar
-                <button type= "submit" >Redirect to Navbar</button>
-            </div>
-        )
-    }
-    
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li className="active">
+            <a>Home</a>
+          </li>
+          <li>
+            <a>News</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 export default Navbar;
