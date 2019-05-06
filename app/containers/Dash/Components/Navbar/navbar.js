589e9201c0d645ea876fdc94d679fa55cda5eab2
aashish <aashishshrestha12@gmail.com>
2019-05-06 12:44:41 +0545

    updated

--- app/containers/Dash/Components/Navbar/navbar.js
+++ app/containers/Dash/Components/Navbar/navbar.js
@@ -3,30 +3,35 @@ import React, { Component } from 'react';
 import './navbar.css';
 
 class Navbar extends Component {
-    logout(){
-        localStorage.removeItem("token");
-        history.pushState("/");
-    }
+  logout() {
+    localStorage.removeItem('token');
+    history.pushState('/');
+  }
   render() {
     return (
-      <div>
-        <ul>
-          <li className="active">
-            <a>Home</a>
-          </li>
-          <li>
-            <a>News</a>
-          </li>
-          <li>
-            <a>About</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
-          <li>
-            <a onClick={this.logout}>Logout</a>
-          </li>
-        </ul>
+      <div className="ui inverted segment">
+        <div className="ui inverted secondary menu">
+          <ul>
+            <li className="active item">
+              <a>Home</a>
+            </li>
+            <li className = "item">
+              <a>News</a>
+            </li>
+            <li className="item">
+              <a>About</a>
+            </li>
+            <li className="item">
+              <a>Contact</a>
+            </li>
+            <li className="item">
+              <a onClick={this.logout}>Logout</a>
+            </li>
+            <li className="item">
+            <a ></a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
